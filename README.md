c4bc32c1c32068696e3e895f50065e5f90cc729f
suhpark <suhpark@gmarket.com>
2022-11-03 17:04:55 +0900

    first try

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # celsius_to_fahrenheit
 파이토치 딥러닝 마스터
+
+ref) https://github.com/onlybooks/pytorch/blob/master/p1ch5/1_parameter_estimation.ipynb
--- main.py
+++ main.py
@@ -1,16 +1,79 @@
-# This is a sample Python script.
+import torch
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+t_c = [0.5, 14.0, 15.0, 28.0, 11.0, 8.0, 3.0, -4.0, 6.0, 13.0, 21.0]
+t_u = [35.7, 55.9, 58.2, 81.9, 56.3, 48.9, 33.9, 21.8, 48.4, 60.4, 68.4]
+t_c = torch.tensor(t_c)
+t_u = torch.tensor(t_u)
+
+def model(t_u, w, b):
+    return w * t_u + b
+
+def loss_fn(t_p, t_c):
+    squared_diffs = (t_p - t_c)**2
+    return squared_diffs.mean()
+
+w = torch.ones(())
+b = torch.zeros(())
+
+t_p = model(t_u, w, b)
+# print(t_p)
+
+loss = loss_fn(t_p, t_c)
+# print(loss)
+
+delta = 0.1
+loss_rate_of_change_w = (loss_fn(model(t_u, w + delta, b), t_c) - loss_fn(model(t_u, w - delta, b), t_c)) / (2.0 * delta)
+learning_rate = 1e-2
+w = w - learning_rate * loss_rate_of_change_w
+
+loss_rate_of_change_b = (loss_fn(model(t_u, w, b + delta), t_c) - loss_fn(model(t_u, w, b - delta), t_c)) / (2.0 * delta)
+b = b - learning_rate * loss_rate_of_change_b
+
+def dloss_fn(t_p, t_c):
+    dsq_diffs = 2 * (t_p - t_c) / t_p.size(0)
+    return dsq_diffs
+
+def dmodel_dw(t_u, w, b):
+    return t_u
+
+def dmodel_db(t_u, w, b):
+    return 1.0
+
+def grad_fn(t_u, t_c, t_p, w, b):
+    dloss_dtp = dloss_fn(t_p, t_c)
+    dloss_dw = dloss_dtp * dmodel_dw(t_u, w, b)
+    dloss_db = dloss_dtp * dmodel_db(t_u, w, b)
+    return torch.stack([dloss_dw.sum(), dloss_db.sum()])
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def training_loop(n_epochs, learning_rate, params, t_u, t_c, print_params=True):
+    for epoch in range(1, n_epochs + 1):
+        w, b = params
 
+        t_p = model(t_u, w, b)  # 순방향 전달
+        loss = loss_fn(t_p, t_c)
+        grad = grad_fn(t_u, t_c, t_p, w, b)  # 역방향 전달
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+        params = params - learning_rate * grad
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        if epoch in {1, 2, 3, 10, 11, 99, 100, 4000, 5000}:  #
+            print('Epoch %d, Loss %f' % (epoch, float(loss)))
+            if print_params:
+                print('    Params:', params)
+                print('    Grad:  ', grad)
+        if epoch in {4, 12, 101}:
+            print('...')
+
+        if not torch.isfinite(loss).all():
+            break  # <3>
+
+    # return params
+    print(params)
+
+training_loop(
+    n_epochs=100,
+    learning_rate=1e-2,
+    params=torch.tensor([1.0, 0.0]),
+    t_u=t_u,
+    t_c=t_c
+)
