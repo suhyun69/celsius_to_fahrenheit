67b019abb4c3ac93526d88a95fb5d98db484b69e
suhpark <suhpark@gmarket.com>
2022-11-03 17:25:27 +0900

    시각화

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import torch
+from matplotlib import pyplot as plt
 
 t_c = [0.5, 14.0, 15.0, 28.0, 11.0, 8.0, 3.0, -4.0, 6.0, 13.0, 21.0]
 t_u = [35.7, 55.9, 58.2, 81.9, 56.3, 48.9, 33.9, 21.8, 48.4, 60.4, 68.4]
@@ -67,12 +68,12 @@ def training_loop(n_epochs, learning_rate, params, t_u, t_c, print_params=True):
         if not torch.isfinite(loss).all():
             break  # <3>
 
-    # return params
-    print(params)
+    return params
+    # print(params)
 
 t_un = 0.1 * t_u
 
-training_loop(
+params = training_loop(
     n_epochs=5000,
     learning_rate=1e-2,
     params=torch.tensor([1.0, 0.0]),
@@ -80,3 +81,13 @@ training_loop(
     t_c=t_c,
     print_params=False
 )
+print(params)
+
+t_p = model(t_un, *params)  # 단위를 모르는 값을 정규화하여 훈련하고 있음. 인자도 언패킹하고 있음
+
+fig = plt.figure(dpi=600)
+plt.xlabel("Temperature (°Fahrenheit)")
+plt.ylabel("Temperature (°Celsius)")
+plt.plot(t_u.numpy(), t_p.detach().numpy()) # 알 수 없는 원본 값을 그려보고 있음
+plt.plot(t_u.numpy(), t_c.numpy(), 'o')
+plt.savefig("temp_unknown_plot.png", format="png")  # bookskip
