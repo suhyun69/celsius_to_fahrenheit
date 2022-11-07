e2f1ce84799e3bc513a9904045fb0c5e36b26593
suhpark <suhpark@gmarket.com>
2022-11-07 23:06:15 +0900

    데이터 변환

--- bird_or_airplane.py
+++ bird_or_airplane.py
@@ -1,6 +1,7 @@
 import torch
 from torchvision import datasets
 from matplotlib import pyplot as plt
+from torchvision import transforms
 
 data_path = '../data-unversioned/p1ch7/'
 cifar10 = datasets.CIFAR10(data_path, train=True, download=True) # <1>
@@ -10,4 +11,13 @@ print(len(cifar10))
 
 img, label = cifar10[99]
 plt.imshow(img)
+# plt.show()
+
+to_tensor = transforms.ToTensor()
+img_t = to_tensor(img)
+
+tensor_cifar10 = datasets.CIFAR10(data_path, train=True, download=False, transform=transforms.ToTensor())
+imt_t, _ = tensor_cifar10[99]
+
+plt.imshow(img_t.permute(1,2,0)) # C x H x W를 H x W x C로 바꿔준다
 plt.show()
