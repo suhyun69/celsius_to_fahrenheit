f0876c27194977f0277b567691ddc2fc7c66c0cb
suhpark <suhpark@gmarket.com>
2022-11-07 23:35:31 +0900

    새와 비행기를 구별하기 : 데이터셋 구축

--- bird_or_airplane.py
+++ bird_or_airplane.py
@@ -1,9 +1,9 @@
 import torch
-from torchvision import datasets
+from torchvision import datasets, transforms
 from matplotlib import pyplot as plt
-from torchvision import transforms
 
 data_path = '../data-unversioned/p1ch7/'
+'''
 cifar10 = datasets.CIFAR10(data_path, train=True, download=True) # <1>
 cifar10_val = datasets.CIFAR10(data_path, train=False, download=True) # <2>
 
@@ -49,3 +49,33 @@ transformed_cifar10 = datasets.CIFAR10(
 img_t, _ = transformed_cifar10[99]
 plt.imshow(img_t.permute(1,2,0))
 plt.show()
+'''
+
+cifar10 = datasets.CIFAR10(
+    data_path,
+    train=True,
+    download=False,
+    transform=transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4915, 0.4823, 0.4468),
+                             (0.2470, 0.2435, 0.2616))
+    ]))
+
+cifar10_val = datasets.CIFAR10(
+    data_path,
+    train=False,
+    download=False,
+    transform=transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4915, 0.4823, 0.4468),
+                             (0.2470, 0.2435, 0.2616))
+    ]))
+
+label_map = {0: 0, 2: 1}
+class_names = ['airplane', 'bird']
+cifar2 = [(img, label_map[label])
+          for img, label in cifar10
+          if label in [0, 2]]
+cifar2_val = [(img, label_map[label])
+              for img, label in cifar10_val
+              if label in [0, 2]]
